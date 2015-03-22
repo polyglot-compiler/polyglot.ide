b6baabca93c5a1f498ded052ea13fb8f2a6ae411
Karthik Bellur <kvb26@cornell.edu>
2015-03-22 16:55:28 -0400

    Minor improvements to Polyglot IDE

--- src/polyglot/ide/natures/JLNature.java
+++ src/polyglot/ide/natures/JLNature.java
@@ -4,10 +4,12 @@ import org.eclipse.core.runtime.CoreException;
 
 public class JLNature extends AbstractNature {
 
+  public static final String NATURE_ID = "polyglot.ide.jlnature";
+
   @Override
   public void configure() throws CoreException {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
